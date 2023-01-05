4db8ea5bb8f23646541d9e850425a156b2ddfe1b
Andy Yang <yangandy1001@gmail.com>
2023-01-05 15:35:52 -0500

    added dependecies alan ai, material ui, words-to-numbers, classnames, starter files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+/*
+    This is our application's top-level component.
+    
+    @author Andy Yang
+*/
+/*
+  This is the entry-point for our application. Many different home pages
+  
+  @author Andy Yang
+*/
+
+import React from 'react'
+import alanBtn from "@alan-ai/alan-sdk-web";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        HELLLOOO
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
