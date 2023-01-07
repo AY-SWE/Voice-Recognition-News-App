1a597904e63500c0b090f8c385f61e925f319fe0
Andy Yang <yangandy1001@gmail.com>
2023-01-07 13:30:53 -0500

    still working on news card css

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 ## A voice-recognition React application powered by Alan AI that features a variety of news articles. User will voice commands that will prompt the desired news articles. Have fun exploring!
 
 Technologies used:
-Frontend: Javascript, HTML/CSS, React.js, Material UI
+Frontend: Javascript, HTML/CSS, React.js, Material UI<br />
 Backend: Alan AI, News API
--- src/components/newsCard/NewsCard.jsx
+++ src/components/newsCard/NewsCard.jsx
@@ -5,23 +5,17 @@ const NewsCard = ({
   i,
 }) => {
   return (
-    <div class="container">
-      <div class="card">
-        <div class="card-header">
-          <img
-            src="https://c0.wallpaperflare.com/preview/483/210/436/car-green-4x4-jeep.jpg"
-            alt="rover"
-          />
+    <div class={css.container}>
+      <div class={css.card}>
+        <div class={css.cardHeader}>
+          <img src={urlToImage} alt="" />
         </div>
+
         <div class="card-body">
           <span class="tag tag-teal">Technology</span>
           <h4>Why is the Tesla Cybertruck designed the way it is?</h4>
           <p>An exploration into the truck's polarising design</p>
           <div class="user">
-            <img
-              src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo"
-              alt="user"
-            />
             <div class="user-info">
               <h5>July Dec</h5>
               <small>2h ago</small>
